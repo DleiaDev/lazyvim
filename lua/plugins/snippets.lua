fe8580f03bbee91fd05f34086e0ea2986687dce6
DleiaDev <markowebdeveloper@gmail.com>
2024-10-15 20:14:31 +0200

    add: custom snippets

---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,15 @@
+return {
+  "garymjr/nvim-snippets",
+  opts = function(_, opts)
+    if opts.search_paths == nil then
+      opts.search_paths = {}
+    end
+    table.insert(opts.search_paths, vim.fn.stdpath("data") .. "/lazy/vscode-vitest-snippets")
+    table.insert(opts.search_paths, vim.fn.stdpath("data") .. "/lazy/vscode-testing-library-snippets")
+    vim.print(_)
+  end,
+  dependencies = {
+    "deinsoftware/vscode-vitest-snippets",
+    "deinsoftware/vscode-testing-library-snippets",
+  },
+}
