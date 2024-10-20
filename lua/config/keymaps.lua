734541dc1f779a375e966591a1e7b1438e4f983b
DleiaDev <markowebdeveloper@gmail.com>
2024-10-20 11:27:17 +0200

    add: Lazydocker and Lazysql

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.del("n", "<leader>l")
+vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- Lazydocker
+if vim.fn.executable("lazydocker") == 1 then
+  vim.keymap.set("n", "<leader>ld", function()
+    LazyVim.terminal("lazydocker", { esc_esc = false, ctrl_hjkl = false })
+  end, { desc = "Lazydocker" })
+end
+
+-- Lazysql
+if vim.fn.executable("lazysql") == 1 then
+  vim.keymap.set("n", "<leader>ls", function()
+    LazyVim.terminal("lazysql", { esc_esc = false, ctrl_hjkl = false })
+  end, { desc = "Lazysql" })
+end
